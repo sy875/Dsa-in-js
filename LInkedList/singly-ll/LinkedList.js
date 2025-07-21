9110f5703c2fa8b87e644a959c2fa4661c63377c
sy875 <sonaly647@gmail.com>
2025-07-21 14:36:21 +0530

    feat:learned about basics of singly linked list

---
+++ LInkedList/singly-ll/LinkedList.js
@@ -0,0 +1,146 @@
+import Node from "./Node.js";
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = this.head;
+    this.size = 0;
+  }
+
+  addFirst(val) {
+    let newNode = new Node(val);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.size++;
+      return;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+    // head->newNode
+    // prevNode-> prevhead
+  }
+
+  addLast(val) {
+    let newNode = new Node(val);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.size++;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size++;
+  }
+
+  add(idx, val) {
+    if (this.head == null) {
+      this.addFirst(val);
+      return;
+    }
+    if (this.size <= idx) {
+      this.addLast(val);
+      return;
+    }
+    let temp = this.head;
+    while (temp != null && --idx > 0) {
+      temp = temp.next;
+    }
+
+    let newNode = new Node(val);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.size++;
+  }
+  printLL() {
+    let temp = this.head;
+    while (temp != null) {
+      console.log(temp.val, "-->");
+      temp = temp.next;
+    }
+  }
+  removeFirst() {
+    if (this.head == null) return;
+    if (this.head.next == null) {
+      this.head = null;
+      this.size--;
+      return;
+    }
+    this.head = this.head.next;
+    this.size--;
+  }
+  removeLast() {
+    if (this.head == null) return;
+    if (this.head == this.tail) {
+      this.head = null;
+      this.size -= 1;
+      return;
+    }
+    let temp = this.head;
+    while (temp.next != this.tail) {
+      temp = temp.next;
+    }
+    temp.next = null;
+    this.tail = temp;
+    this.size -= 1;
+  }
+  remove(idx) {
+    if (idx == 1) {
+      this.removeFirst();
+      return;
+    }
+    if (idx == this.size) {
+      this.removeLast();
+      return;
+    }
+    let temp = this.head;
+    while (temp.next != null && --idx) {
+      temp = temp.next;
+    }
+
+    temp.next = temp.next.next;
+    this.size--;
+  }
+
+  search(val) {
+    if (this.head == null) return;
+    let temp = this.head;
+    let idx = 0;
+  
+    while (temp != null) {
+      if(temp.val == val) return idx
+      temp = temp.next;
+      idx++;
+    }
+    return -1;
+  }
+}
+
+let ll = new LinkedList();
+
+ll.addFirst(20);
+ll.addLast(30);
+ll.addLast(40);
+ll.addFirst(10);
+ll.addLast(50);
+ll.add(2, 44);
+
+console.log(ll.printLL());
+// ll.removeFirst();
+// ll.removeLast();
+// ll.remove(2);
+console.log(ll.printLL());
+console.log("the val 44 found at idx", ll.search(44))
+// console.log(
+//   "head is ",
+//   ll.head.val,
+//   "tail is ",
+//   ll.tail.val,
+//   "size of linkd list is ",
+//   ll.size
+// );
+
+
