4f8c9efdbb13e4a3292fa7e2bd0dfcbd7e0c7f66
sy875 <sonaly647@gmail.com>
2025-07-24 13:14:50 +0530

    feat: added qs leetcode-796 rotate string

---
+++ Strings/Easy/rotate-string.js
@@ -0,0 +1,14 @@
+const rotateString = function (s, goal) {
+  if (goal.length !== s.length) return false;
+  let newstr = s + s;
+
+  for (let i = 0; i < s.length; i++) {
+    let slice = newstr.slice(i, i + goal.length);
+    if (slice === goal) return true;
+  }
+
+  return false;
+};
+
+let s = "abcde",goal = "cdeab";
+console.log(rotateString(s, goal));
